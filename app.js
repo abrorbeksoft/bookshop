02667af34dad27f8f729bb9febc39a2bf70302ee
Abrorbek Ubaydullayev <abrorbek.ubaydullayev98@gmail.com>
2021-04-17 20:40:06 +0500

    Create simple app

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express=require("express");
 const app=express();
-const port=3000;
+const PORT=process.env.PORT || 80;
 
 app.get("/",(req ,res)=>{
-
-    res.status(200).json({ name:"Abrorbek" })
-
+    res.end(`
+        <div>
+            <ul>
+                <li><a href="/" >Home</a></li>
+                <li><a href="/about" >About</a></li>
+            </ul>
+            <h1>Home page</h1>
+        </div>
+    `)
 })
 
-app.listen(port,()=>{
+app.get("/about",(req,res)=>{
+    res.end(`
+        <div>
+            <ul>
+                <li><a href="/" >Home</a></li>
+                <li><a href="/about" >About</a></li>
+            </ul>
+            <h1>About page</h1>
+        </div>
+    `)
+})
+
+app.listen(PORT,()=>{
     console.log("Server is started")
 })
